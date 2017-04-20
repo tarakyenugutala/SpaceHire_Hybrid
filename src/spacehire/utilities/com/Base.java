b15a30a6380242fbbebe32cdf4cbafa9a67d66c4
Suji Naresh <Suji Naresh@DESKTOP-2DPTTHT>
2017-04-20 19:36:45 +0530

    Added reusable methods

--- src/spacehire/utilities/com/Base.java
+++ src/spacehire/utilities/com/Base.java
@@ -1,5 +1,28 @@
 package spacehire.utilities.com;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class Base {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void preCondition()
+	{
+		driver = new FirefoxDriver();
+		driver.get("http://www.way2automation.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@AfterClass
+	public void postCondition()
+	{
+		driver.close();
+	}
+	
 
 }
